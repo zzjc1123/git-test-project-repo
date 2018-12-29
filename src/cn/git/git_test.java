4a53d131bcc26ca08321f71020469a3e5694d4c8
孙麟 <wangyisunlin@163.com>
2018-12-29 16:44:15 +0800

    提交git到本地库

---
+++ src/cn/git/git_test.java
@@ -0,0 +1,14 @@
+package cn.git;
+
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+        System.out.println("hello git!!");
+    }
+}
